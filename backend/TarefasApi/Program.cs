2182ee08e3766b6df6b1f47a79e3935d5789baf9
BrenoMelo0 <brenomeloao00@gmail.com>
2025-07-16 17:53:14 -0300

    fsdfs

---
+++ backend/TarefasApi/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Habilita Controllers
+builder.Services.AddControllers();
+
+// Habilita CORS (para o frontend acessar)
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+// Habilita Swagger (ESSENCIAL pra evitar o erro)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
